1c1d35ed33c29c866cce13ac740cb0396514d938
yben352 <yben352@gmail.com>
2024-12-30 14:39:23 +0200

    send PublicIPChecker.py script

---
+++ PublicIPChecker.py
@@ -0,0 +1,11 @@
+import requests
+
+def get_public_ip():
+    try:
+        response = requests.get("https://api.ipify.org?format=json")
+        return response.json()["ip"]
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    print("Your public IP address is:", get_public_ip())
